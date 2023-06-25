8e1bfb2af903893f54cf0a167fd03919cd6daa24
AllahverdiyevaS <135016767+AllahverdiyevaS@users.noreply.github.com>
2023-06-25 08:48:26 +0000

    Add files via upload
    Homework

---
+++ script.js
@@ -0,0 +1,43 @@
+// Напишите цикл for, который выводит консоль каждое второе число от 0 до 10
+
+// const arr=[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+// console.log(arr [i])
+
+// ==============================
+
+// Напишите цикл for, который выводит в консоль  все числа от 55 до 20
+
+// for (let i = 55; i >=20; i = i -1) 
+// console.log(i)
+// ===========================
+
+// Дан массив numbers. Вывести в консоль все числа из массива
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6];
+
+// Сформировать новый массив  и передать в него все элементы из массива numbers, возведенные в квадрат
+// const numbers = [3, 5, 11, 2, 8, 1, 6];
+// for (let i = 0; i < numbers.length; i = i + 1) 
+// numbers_squared=[numbers[0]**2,numbers[1]**2,numbers[2]**2,numbers[3]**2,numbers[4]**2,numbers[5]**2,numbers[6]**2]
+// console.log( numbers_squared)
+
+// ==============
+// Создать переменную last_elem и передать в нее последний элемент из сформированного массива numbers_squared (обратиться к элементу массива по индексу)
+// const numbers_squared=[9, 25, 121, 4, 64, 1, 36]
+// let last_elem=numbers_squared[numbers_squared.length -1]
+// console.log(last_elem)
+// ===============
+
+// Дан объект user. Используя данные из объекта, сформировать строку в формате: ‘User’s name is <FIRST_NAME> <LAST_NAME>. He is <AGE> years old’
+
+// const user = {
+// first_name: 'Ivan', 
+// last_name: 'Ivanov', 
+// age: 20, 
+// salary: 500
+// }
+// const{first_name,last_name,age}=user
+
+// console.log(`Users name is ${first_name} ${last_name}. He is ${age} years old`)
+
